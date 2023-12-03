4059f05f53bc5f18d8eb471f595232ddc3aa7bee
Luis García <luisgc1@gmail.com>
2023-12-03 18:13:20 +0100

    Vamos introduciendo código en el .js

--- scripts/script.js
+++ scripts/script.js
@@ -2,7 +2,24 @@
 ** Autor :Luis García 
 **GitHub:https://github.com/LuisGarCas
 */
+// Enlazamos la pantalla con el javascript
 
 let display = document.getElementById("display")
 
+// variable de scope global del valor actual de la pantalla
+
 let currentInput = "0"
+
+function clearDisplay() {
+    currentInput = "0"
+    display.value = currentInput
+}
+const appendToDisplay = (number) => {
+
+}
+function insertPI(){
+    currentInput = Math.PI.toString
+    display.value = currentInput
+}
+
+
