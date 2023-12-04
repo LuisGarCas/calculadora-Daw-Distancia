4011920bae440d79a9b631d62ca9deb6be0a1ce7
Luis García <luisgc1@gmail.com>
2023-12-04 17:10:37 +0100

    Segruimos introduciendo código en el .js

--- scripts/script.js
+++ scripts/script.js
@@ -14,12 +14,48 @@ function clearDisplay() {
     currentInput = "0"
     display.value = currentInput
 }
-const appendToDisplay = (number) => {
+function appendToDisplay(value) {
+    // Si el valor actual es 0, significa que no hemos pulsado nada.
+    if (currentInput === "0") {
+        currentInput = ""
+    }
+    currentInput += value
+
+    display.value = currentInput
 
 }
-function insertPI(){
+
+function insertPI() {
     currentInput = Math.PI.toString
     display.value = currentInput
 }
 
+function square() {
+    try {
+        currentInput = Math.pow(parseFloat(currentInput), 2)
+        display.value = currentInput
+
+    } catch (error) {
+        display.value = "Error"
+    }
+}
+
+function changeSign() {
+    try {
+        currentInput = -parseFloat(currentInput)
+        display.value = currentInput
+    } catch (error) {
+        display.value = "Error"
+    }
+}
+
+function division() {
+    try{
+        currentInput = Math.floor(parseFloat(currentInput))
+        display.value= currentInput
+
+    } catch (error) {
+        display.value= "Error"
+    }
+}
 
