9ef6e05e7277344a41c2afad6b64bba77dbb35dd
Luis García <luisgc1@gmail.com>
2023-12-14 11:53:41 +0100

    terminamos las funciones .js

--- scripts/script.js
+++ scripts/script.js
@@ -48,26 +48,39 @@ function changeSign() {
         display.value = "Error"
     }
 }
-// funcióon mía
+// función mía
 
-function division() {
-    try{
-        currentInput = Math.floor(parseFloat(currentInput))
-        display.value= currentInput
-
-    } catch (error) {
-        display.value= "Error"
-    }
-}
-function add(){
+function calculateResult() {
     try {
-        currentInput= parseFloat(currentInput)
-        display.value=currentInput
-    }catch(error) {
-        display.value ="Error"
+        // 5 -------------------------------> 10/2
+        currentInputResult = eval(currentInput)
+        console.log(currentInputResult)
+        display.value = currentInputResult
+    } catch (error) {
+        display.value = "Error"
     }
 }
 
+function backSpace(){
+    currentInput=display.value
+    currentInput=currentInput.substring(0,currentInput.length-1)
+    display.value=currentInput
+}
 
+function reciprocal(){
+    currentInput=display.value
+    currentInput=1/currentInput
+    display.value=currentInput
+}
+function calculatePercentage(){
+    currentInput=display.value
+    currentInput=currentInput/100
+    display.value=currentInput
+}
+function root(){
+    currentInput=display.value
+    currentInput=Math.sqrt(currentInput,2)
+    display.value=currentInput
+}
 
 
